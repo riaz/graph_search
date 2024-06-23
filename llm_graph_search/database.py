92e1caee1e08668d9e3abba9225ac047741a73bb
riaz <riaz.2012@gmail.com>
2024-06-23 00:09:38 -0700

    remoing gremlin, looks like need to use in-memory graphdb

--- llm_graph_search/database.py
+++ llm_graph_search/database.py
@@ -1,8 +1,6 @@
 import os
 from dotenv import load_dotenv
 from neo4j import GraphDatabase
-from gremlinpython.driver import client, serializer
-from gremlinpython.driver.protocl import GremlinServerError
 
 
 # Note: we will make use of a env file to fetch the server configuration we are looking for
@@ -17,12 +15,6 @@ class GraphDatabase():
             user = os.getenv("NEO4J_USER")
             password = os.getenv("NEO4J_PASSWORD")
             self.driver = GraphDatabase.driver(uri, auth=(user, password))
-
-        elif self.db_type == "neptune":
-            endpoint = os.getenv("NEPTUNE_ENDPOINT")
-            port = os.getenv("NEPTUNE_PORT")
-            self.client = client.Client(f"wss://{endpoint}:{port}/gremlin", "g", 
-                                        message_serializer=serializer.GraphSONSerializersV2d0())
         else:
             # an invalid db type was encountered
             raise ValueError("Invalid database type specified")
@@ -30,14 +22,9 @@ class GraphDatabase():
     def close(self):
         if self.db_type == "neo4j":
             self.driver.close()
-        elif self.db_type == "neptune":
-            self.client.close()
 
     def execute_query(self, query, params=None):
         if self.db_type == "neo4j":
             with self.driver.session() as session:
                 result = session.run(query, params)
                 return [record.data() for record in result]
-        elif self.db_type == "neptune":
-            result = self.client.submit(query, params)
-            return [item.as_dict() for item in result]
